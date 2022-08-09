dd8fd573b19d6cab7d6cd3758d0a0d3c33d20e72
NataliaDomanova <n.domanova@mail.ru>
2022-08-10 00:06:05 +0300

    Changes in mole-game

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,47 @@
+
+let dead = document.getElementById("dead");
+let lost = document.getElementById("lost");
+
+for (let index = 1; index < 10; index++) {
+ 
+	function getHole (index) {
+		let holeClick = document.getElementById(`hole${index}`);
+		return holeClick;
+	}
+		let hole = getHole (index);
+
+	hole.onclick = function() {
+		if (hole.className.includes("hole_has-mole") === true) {
+			dead.textContent ++;
+      
+		} else {
+			lost.textContent ++;
+    }
+
+    if (dead.textContent == 10 ) {
+      alert ("Вы победили!"); 
+      dead.textContent = 0;
+      lost.textContent = 0;
+    } 
+    if (lost.textContent == 5) {
+        alert ("Вы проиграли");
+        dead.textContent = 0;
+        lost.textContent = 0;
+        }
+    }
+
+  }
+
+/*
+		if (dead.textContent === 10) {
+      return;
+			alert ("Вы победили!");
+			dead = 0;
+			lost = 0;
+		} else if (lost.textContent === 5) {
+			alert ("Вы проиграли");
+			dead = 0;
+			lost = 0;
+			}
+	}
+*/
