cb8a12960777216b0147c3fb3f0963243efebe90
NataliaDomanova <n.domanova@mail.ru>
2022-09-07 15:32:01 +0300

    Last changes in reveal

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,10 +1,12 @@
 let isInViewport = function() {
     const div = document.querySelectorAll('.reveal');
     div.forEach((element) => {
-    const top = element.getBoundingClientRect().top;
-    //console.log(top);
-    //console.log(window.innerHeight);    
-        if(top < window.innerHeight) {        
+    const {top, bottom} = element.getBoundingClientRect();
+    
+    console.log(top, bottom);
+    console.log(window.innerHeight);                  
+
+    if(top < window.innerHeight && bottom > 0) {        
             element.classList.add('reveal_active');  
         } else {
             element.classList.remove('reveal_active');
